39d729c1f57293d60ff0bd2b966899342d235e10
Ella <lunar.politics@hmail.com>
2022-12-16 15:30:19 +0300

    OrderSummary added

---
+++ src/components/OrderSummary.vue
@@ -0,0 +1,49 @@
+<!-- eslint-disable max-len -->
+<template>
+  <div>
+    <div class="cart__block">
+      <ul class="cart__orders">
+        <OrderItem v-for="item in products" :key="item.productId" :item="item" />
+      </ul>
+      <div class="cart__total">
+        <p>Доставка: <b>500 ₽</b></p>
+        <p>Итого: <b>{{ products.length }}</b> товара на сумму <b>{{ totalPrice | numberFormat }} ₽</b></p>
+      </div>
+
+      <button class="cart__button button button--primery" type="submit">
+        Оформить заказ
+      </button>
+    </div>
+    <div class="cart__error form__error-block" v-if="formErrorMessage">
+      <h4>Заявка не отправлена!</h4>
+      <p>
+        {{ formErrorMessage }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import numberFormat from '@/helpers/numberFormat';
+import OrderItem from '@/components/OrderItem.vue';
+
+export default {
+  filters: { numberFormat },
+  components: {
+    OrderItem,
+  },
+  props: ['formErrorMessage'],
+  // data() {
+  //   return {
+  //     formErrorMessage: '',
+  //   };
+  // },
+  computed: {
+    ...mapGetters({ products: 'cartDetailProducts', totalPrice: 'cartTotalPrice' }),
+    // formErrorMessage() {
+    //   return this.$store.state.orderInfo.error.response.data.error.message;
+    // },
+  },
+};
+</script>
--- src/pages/OrderPage.vue
+++ src/pages/OrderPage.vue
@@ -83,59 +83,31 @@
             </ul>
           </div>
         </div>
-
-        <div class="cart__block">
-          <ul class="cart__orders">
-            <OrderItem v-for="item in products" :key="item.productId" :item="item" />
-          </ul>
-          <div class="cart__total">
-            <p>Доставка: <b>500 ₽</b></p>
-            <p>Итого: <b>{{ products.length }}</b> товара на сумму <b>{{ totalPrice | numberFormat }} ₽</b></p>
-          </div>
-
-          <button class="cart__button button button--primery" type="submit">
-            Оформить заказ
-          </button>
-        </div>
-        <div class="cart__error form__error-block" v-if="formErrorMessage">
-          <h4>Заявка не отправлена!</h4>
-          <p>
-            {{ formErrorMessage }}
-          </p>
-        </div>
+        <OrderSummary :formErrorMessage="formErrorMessage" />
       </form>
     </section>
   </main>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import numberFormat from '@/helpers/numberFormat';
 import BaseFormText from '@/components/BaseFormText.vue';
 import BaseFormTextarea from '@/components/BaseFormTextarea.vue';
-import OrderItem from '@/components/OrderItem.vue';
+import OrderSummary from '@/components/OrderSummary.vue';
 import axios from 'axios';
 import Preloader from '@/components/Preloader.vue';
 import { API_BASE_URL } from '../config';
 
 export default {
-  filters: { numberFormat },
   components: {
-    BaseFormText, BaseFormTextarea, OrderItem, Preloader,
+    BaseFormText, BaseFormTextarea, OrderSummary, Preloader,
   },
   data() {
     return {
       formData: {},
       formError: {},
-      formErrorMessage: '',
       orderLoading: false,
+      formErrorMessage: '',
     };
-  },
-  computed: {
-    ...mapGetters({ products: 'cartDetailProducts', totalPrice: 'cartTotalPrice' }),
-    productsLoading() {
-      return this.$store.state.productsLoading;
-    },
   },
   methods: {
     order() {
